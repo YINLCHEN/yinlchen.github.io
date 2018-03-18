9e9756b6ff1bdcb1b9d8e2ac7b1e8a5c968e2ad5
陳胤霖 <chenyinlin@YINLCHEN-MBP.local>
2018-03-18 23:31:11 +0800

    fix

--- src/components/Resume/Content.js
+++ src/components/Resume/Content.js
@@ -22,6 +22,16 @@ class Content extends React.Component{
         const aboutElement =  Object.keys(data).map((key,index) => 
             <div key={index}>{data[key].About}</div>
         );
+        const aboutElement2 = "<div>2016年春天，那時候正吹起一股FINTECH金融科技風潮，畢業、退伍後，從花蓮北上台北面試，也懵懵懂懂地加入FUBON IT，開始踏入WEB開發的行列<br /><br />"
+        +"工作環境給我的學習曲線，從一開始上升到後期趨於平緩，學習全端包括JAVA、JavaScript、jQuery；一年後，到了O-BANK IT，多了些空間與想法，從使用者角度需要什麼樣的功能，"
+        +"僅僅是一個上傳Excel並顯示出來，因為表格太寬，我想User應該很需要凍結固定欄位的功能，而目前只有用套件去凍結頂端列，要凍結直行無法實現，就自己動手用JavaScript和jQuery刻出來<br /><br />"
+        +"前端帶給我的成就感更高，而且回饋會即時看到，對於美感也有些想法可以應用在工作上，JavaScript的路也還很長，雖然框架如雨後春筍般，但學習背後的概念才是最重要，"
+        +"就像React一開始接觸完全無法理解，我想技術成就是我想往前端專精的原因!<br /><br />"
+        +"</div>"
+        
+        function createMarkup() {
+            return {__html: aboutElement2};
+        }
 
         return (
             <div className="container">
@@ -29,8 +39,9 @@ class Content extends React.Component{
                     <div className="col-lg-10">
                         <h1>關於我</h1>
                         {aboutElement}
+                        <br/>
+                        <div dangerouslySetInnerHTML={createMarkup()} />
                         <hr/><br/>
-
                         <h1>經驗</h1>
                         <div className="container">
                             <div className="row">
