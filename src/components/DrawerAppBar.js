b427141b66b7a3b4d77c1f272082b84acb5ae502
陳胤霖 <sidneychen137@gmail.com>
2018-04-22 22:54:10 +0800

    fix static background

--- src/components/DrawerAppBar.js
+++ src/components/DrawerAppBar.js
@@ -10,6 +10,11 @@ import {
 import SvgIcon from 'material-ui/SvgIcon';
 import IconButton from 'material-ui/IconButton';
 
+import BackImg1 from '../images/IMG_0977.JPG';
+import BackImg2 from '../images/IMG_2014.JPG';
+import BackImg3 from '../images/IMG_6889.JPG';
+import BackImg4 from '../images/IMG_0835.jpg';
+
 const styles = {
     button: {
         marginTop: 5,
@@ -49,6 +54,7 @@ class DrawerAppBar extends React.Component {
     handleClose = () => this.setState({open: false});
 
     render() {
+        ChangeBackgroundFnc(this.props.count)
         return (
             <div>
                 <AppBar
@@ -93,6 +99,24 @@ class DrawerAppBar extends React.Component {
 function mapStateToProps(state) {
     return {
         count: state.count
+    };
+}
+
+function ChangeBackgroundFnc(params) {
+    switch(params % 4) {
+        case 0:
+            document.body.style.background = "#000 url(" + BackImg1 + ") center center fixed no-repeat"
+            break;
+        case 1:
+            document.body.style.background = "#000 url(" + BackImg2 + ") center center fixed no-repeat"
+            break;
+        case 2:
+            document.body.style.background = "#000 url(" + BackImg3 + ") center center fixed no-repeat"
+            break;
+        case 3:
+            document.body.style.background = "#000 url(" + BackImg4 + ") center center fixed no-repeat"
+            break;
+        default:
     };
 }
 
--- src/components/Home/App.js
+++ src/components/Home/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import DrawerAppBar from '../DrawerAppBar';
 import Header from './Header';
-import StaticBackground from '../StaticBackground';
 
 import '../../css/App.css';
 import '../../css/bootstrap-grid.css';
@@ -11,7 +10,6 @@ class App extends Component {
     render() {
         return (
             <div>
-                <StaticBackground />
                 <MuiThemeProvider >
                     <DrawerAppBar />
                 </MuiThemeProvider >
